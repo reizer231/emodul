45dd672b2a32b2489a784b5e1ae12a175aa83247
reizer231 <40005172+reizer231@users.noreply.github.com>
2022-05-17 11:35:37 +0200

    Update fireup_toggle.py

--- fireup_toggle.py
+++ fireup_toggle.py
@@ -1,33 +1,32 @@
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-options = Options()
-options.headless = True
+opts = Options()
+opts.headless = True
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os, signal
 
+usernameStr= "usrname" #Specify your username
+passwordStr = "passwd" #Specify your password
 
-username = "usrname" #Specify your username
-password = "passwd" #Specify your password
-json1 = "/html/body/div/div/div[2]/div/div/div/div[2]/div/div[3]/div/div[1]/tile/div"
-json2 = "/html/body/div/div/div[2]/div/div/div/div[1]/div/div[2]/div/button[3]"
-username_input = "/html/body/div/div/div[2]/div/div/div[2]/form/input[1]"
-password_input = "/html/body/div/div/div[2]/div/div/div[2]/form/input[2]"
-login_button = "/html/body/div/div/div[2]/div/div/div[2]/form/input[3]"
+browser = webdriver.Firefox(options=opts, executable_path ="/usr/local/bin/geckodriver")
+browser.get(('https://emodul.pl/login'))
 
-url = "https://emodul.pl/login"
-driver = webdriver.Firefox(firefox_options=options, executable_path ="/usr/local/bin/geckodriver")
-driver.get(url)
-driver.implicitly_wait(10)
-driver.find_element_by_xpath(username_input).send_keys(username)
-driver.implicitly_wait(10)
-driver.find_element_by_xpath(password_input).send_keys(password)
-driver.implicitly_wait(10)
-driver.find_element_by_xpath(login_button).click()
-driver.implicitly_wait(10)
-driver.find_element_by_xpath(json1).click()
-driver.implicitly_wait(10)
-driver.find_element_by_xpath(json2).click()
+username = browser.find_element(By.NAME, 'username')
+username.send_keys(usernameStr)
+password = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.NAME, "password")))
+password.send_keys(passwordStr)
+signInButton = browser.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/form/input[3]")
+signInButton.click()
+fireup_stageone=WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/div/div/div/div[2]/div/div[3]/div/div[1]/tile/div/div")))
+fireup_stageone.click()
+fireup_stagetwo=WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/div/div/div/div[1]/div/div[2]/div/button[3]")))
+fireup_stagetwo.click()
 
-#Killing all firefox processes as in my case driver.close() was not working.
+#killing firefox in environment -somehow driver.close() not working
 name = "firefox"
 for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
     fields = line.split()
@@ -36,4 +35,6 @@ for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
     # terminating process
     os.kill(int(pid), signal.SIGKILL)
 
-print("Done! Toggled firing up/down!") 
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Done! Toggled firing up/down!"] 
